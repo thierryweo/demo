1bcf58935a4361c299bc19b080a2c4ccd32e476d
Thierry Nicola <tnicola@weo-water.com>
2023-03-14 09:48:10 +0100

    With GH Pages

--- gh-pages.js
+++ gh-pages.js
@@ -1,7 +1,8 @@
-var ghpages = require('gh-pages');
+import ghpages from 'gh-pages';
+
 
 ghpages.publish(
-    'public', // path to public directory
+    'build', // path to public directory
     {
         branch: 'gh-pages',
         repo: 'https://github.com/thierryweo/demo.git', // Update to point to your repository  
